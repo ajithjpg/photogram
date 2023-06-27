c9657aed0145d0de28715fda3ef67fa75a398031
dilli <dilli@howdy.do>
2023-06-27 09:58:13 +0530

    upload setup

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AppState } from '../../app.service'
 import { Router } from '@angular/router';
 
@@ -23,23 +23,51 @@ export class DashboardComponent implements OnInit {
     { name: 'dilli', post: '../../../assets/img/feed-4.jpg', profile_img: '../../../assets/img/feed-4.jpg', post_desc: '' },
     { name: 'dilli', post: '../../../assets/img/feed-5.jpg', profile_img: '../../../assets/img/feed-5.jpg', post_desc: '' },
   ];
+  selectedFile = [];
+  imageUrl;
+  imageshow = 0;
+  desc = '';
   constructor(
-    public router:Router,
-    public appstate:AppState
-  ){
+    public router: Router,
+    public appstate: AppState,
+    private cd: ChangeDetectorRef
+  ) {
 
   }
   ngOnInit(): void {
     var action = window.location.pathname.split('/');
     this.action = action[1];
 
-    if(this.action =='home'){
+    if (this.action == 'home') {
 
     }
 
     var action = window.location.pathname.split('/');
     this.url = action[1];
     this.route_change(this.url);
+  }
+
+  onFileSelected(event: any) {
+    this.selectedFile = [];
+    this.selectedFile.push(event.target.files[0]);
+    console.log(this.selectedFile);
+    this.image_render(event);
+  }
+
+  image_render(event) {
+    this.imageshow = 1;
+    let reader = new FileReader(); // HTML5 FileReader API
+    let file = event.target.files[0];
+    if (event.target.files && event.target.files[0]) {
+      reader.readAsDataURL(file);
+
+      // When file uploads set it to file formcontrol
+      reader.onload = () => {
+        this.imageUrl = reader.result;
+      }
+      // ChangeDetectorRef since file is loading outside the zone
+      this.cd.markForCheck();
+    }
   }
 
   route_change(type: any) {
@@ -84,4 +112,19 @@ export class DashboardComponent implements OnInit {
       this.router.navigate(["/profile"]);
     }
   }
+
+  create_post() {
+
+    const params = new FormData();
+
+    params.append('desc', this.desc);
+    params.append('file', this.selectedFile[0]);
+
+    this.appstate.postMethod(params,'post/1','').subscribe(res =>{
+      
+    })
+  }
+
 }
+
+
